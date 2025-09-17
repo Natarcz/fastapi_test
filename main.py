e5b79c2b647341e9c01d4edbed0319eb293ce7e4
Natarcz <arczukevich1@gmail.com>
2025-09-17 21:06:24 +0300

    add endpoint get_user_by_id

--- main.py
+++ main.py
@@ -3,13 +3,28 @@ from fastapi import FastAPI
 
 app = FastAPI()
 
-@app.get("/users")
-def get_users():
-    return [{"id":1,"name":"Some","email":"Some"}]
+users=[
+    {
+        "id": 1,
+        "username": "admin",
+        "email": "admin@localhost",
+        "password": "admin123",
+    },
+    {
+        "id": 2,
+        "username": "user",
+        "email": "user@localhost",
+        "password": "user123",
+    }
+]
 
 @app.get("/users")
-def get_users2():
-    return [{"id":1,"name":"Some","email":"Some"}]
+def get_users():
+    return users
+
+@app.get("/user_by_id/{id}")
+def get_users2(id:int):
+    return users[id]
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
