940441e1f1b3d2ba69fb01007576c6daea7b8b25
IlyaDevOps <ilya.romanovsky@noogadev.com>
2025-09-17 15:07:40 +0300

    first commit

---
+++ main.py
@@ -0,0 +1,11 @@
+import uvicorn
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/users")
+def get_users():
+    return [{"id":1,"name":"Some","email":"Some"}]
+
+if __name__ == "__main__":
+    uvicorn.run("main:app", host="0.0.0.0", port=8000)
