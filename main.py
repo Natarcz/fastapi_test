d75ede35853e00a876a406bf641abeec2036c665
Natarcz <arczukevich1@gmail.com>
2025-09-17 15:51:48 +0300

    initial commit

--- main.py
+++ main.py
@@ -7,5 +7,9 @@ app = FastAPI()
 def get_users():
     return [{"id":1,"name":"Some","email":"Some"}]
 
+@app.get("/users")
+def get_users2():
+    return [{"id":1,"name":"Some","email":"Some"}]
+
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=8000)
